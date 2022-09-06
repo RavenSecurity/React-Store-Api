2cee8c37f7acd5833e94bb1dc6a735da6b993b14
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-09-06 23:18:14 +0200

    comments

--- index.js
+++ index.js
@@ -20,17 +20,6 @@ app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${P
 
 const MongoClient = require('mongodb').MongoClient;
 
-// MongoClient.connect('mongodb://localhost:27017/animals', (err, client) => {
-//   if (err) throw err;
-
-//   const db = client.db('animals');
-//   db.collection('mammals').find().toArray((err, result) => {
-//     if (err) throw err;
-//     console.log(result);
-//     client.close();
-//   });
-// });
-
 // https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
 
 
